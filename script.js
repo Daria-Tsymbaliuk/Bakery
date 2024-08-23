cb753e3e017e2c527b1cd402cb91b3df8115e61c
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2024-08-23 16:08:19 +0200

    Виправляє помилки та ще трішки додає js

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ buttonEl.addEventListener("click", () => {
     iframeEl.style.filter = "invert(100%)";
   }
 });
+// scroll btn
+const backToTopBtn = document.getElementById("backToTopBtn");
+const circle = document.querySelector('.progress-ring__circle');
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = circumference;
+
+window.onload = function() {
+    // Скрити кнопку при загрузке сторінки
+    backToTopBtn.style.display = "none";
+};
+
+window.onscroll = function() {
+    const headerHeight = document.querySelector("header").offsetHeight;
+
+    // Показ кнопки при прокрутке ниже header
+    if (document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) {
+        backToTopBtn.style.display = "block"; // Показати кнопку
+    } else {
+        backToTopBtn.style.display = "none"; // Скрити кнопку
+    }
+
+    // Обновлення прогресса прокрутки
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPosition = document.documentElement.scrollTop;
+    const scrollPercentage = scrollPosition / scrollHeight;
+
+    const offset = circumference - scrollPercentage * circumference;
+    circle.style.strokeDashoffset = offset;
+};
+
+backToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
