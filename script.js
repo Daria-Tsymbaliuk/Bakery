2b0d08c788c3074f0fdf2c5f6d51dcee69aa660e
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2024-10-13 17:20:39 +0200

    трішки додає Modal

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ window.onscroll = function() {
 backToTopBtn.onclick = function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
